9c1f9ecb32d7377dcb09bd45275b376c8c6ff265
Haraldur <haraldur@live.dk>
2023-09-22 09:59:07 +0200

    TypeScript

--- ClientSide/typescript/out/main.js
+++ ClientSide/typescript/out/main.js
@@ -1,43 +1,119 @@
 "use strict";
-var btnArray = [];
-var Button = /** @class */ (function () {
-    function Button(studentId, name) {
-        var _a;
-        this.studentId = studentId;
-        this.name = name;
-        this.btn = document.getElementById('btnTs');
-        (_a = this.btn) === null || _a === void 0 ? void 0 : _a.addEventListener('click', this.create);
+var Calculator = /** @class */ (function () {
+    // constructor
+    function Calculator() {
+        var _this = this;
+        // initailize variables
+        this.currentInput = "";
+        this.previousInput = "";
+        this.currentOperation = null;
+        this.currentOperand = null;
+        this.previousOperand = null;
+        // get elements
+        this.currentOperand = document.getElementById("current-operand");
+        this.previousOperand = document.getElementById("previous-operand");
+        var clearButton = document.getElementById("clear");
+        var deleteButton = document.getElementById("delete");
+        var operationButtons = document.querySelectorAll(".operation-button");
+        var numberButtons = document.querySelectorAll(".number-button");
+        var calculateButton = document.getElementById("calculate");
+        this.updateDisplay();
+        // Button click
+        numberButtons.forEach(function (button) {
+            button.addEventListener("click", function () {
+                _this.numberButtonClick(button.textContent || "");
+            });
+        });
+        operationButtons.forEach(function (button) {
+            button.addEventListener("click", function () {
+                _this.operationButtonClick(button.textContent || "");
+            });
+        });
+        clearButton.addEventListener("click", function () {
+            _this.clear();
+        });
+        deleteButton.addEventListener("click", function () {
+            _this.delete();
+        });
+        calculateButton.addEventListener("click", function () {
+            _this.calculate();
+        });
     }
-    Button.prototype.create = function () {
-        var input = document.getElementById('inputTs');
-        var tableBody = document.getElementById('tableBody');
-        var studentId = btnArray.length + 1;
-        var name = "".concat(input === null || input === void 0 ? void 0 : input.value);
-        //New Row
-        var newRow = document.createElement('tr');
-        newRow.innerHTML = "\n            <td>".concat(studentId, "</td>\n            <td>").concat(name, "</td>\n            <td><button class=\"editBtn\">Edit</button></td>\n            <td><button class=\"deleteBtn\">Delete</button></td>\n        ");
-        //Edit Button
-        var editBtn = newRow.querySelector('.editBtn');
-        if (editBtn) {
-            editBtn.addEventListener('click', function () {
-                console.log("Edit");
-            });
+    Calculator.prototype.updateDisplay = function () {
+        if (this.currentOperand && this.previousOperand) {
+            this.currentOperand.textContent = this.currentInput;
+            if (this.currentOperation) {
+                this.previousOperand.textContent = "".concat(this.previousInput, " ").concat(this.currentOperation);
+            }
+            else {
+                this.previousOperand.textContent = "";
+            }
         }
-        //Delete Button
-        var deleteBtn = newRow.querySelector('.deleteBtn');
-        if (deleteBtn) {
-            deleteBtn.addEventListener('click', function () {
-                console.log("Delete");
-            });
-        }
-        //Append Row
-        if (tableBody) {
-            console.log("hej");
-            tableBody.appendChild(newRow);
-        }
-        var newBtn = new Button(studentId, name);
-        btnArray.push(newBtn);
     };
-    return Button;
+    Calculator.prototype.numberButtonClick = function (value) {
+        if (this.currentInput === "0" && value === "0") {
+            return;
+        }
+        this.currentInput += value;
+        this.updateDisplay();
+    };
+    Calculator.prototype.operationButtonClick = function (operation) {
+        if (this.currentInput === "") {
+            return;
+        }
+        if (this.currentOperation) {
+            this.previousInput = this.currentInput;
+            this.currentInput = "";
+            this.currentOperation = operation;
+            this.updateDisplay();
+        }
+        else {
+            this.currentOperation = operation;
+            this.previousInput = this.currentInput;
+            this.currentInput = "";
+            this.updateDisplay();
+        }
+    };
+    Calculator.prototype.clear = function () {
+        this.currentInput = "";
+        this.previousInput = "";
+        this.currentOperation = null;
+        this.updateDisplay();
+    };
+    Calculator.prototype.delete = function () {
+        this.currentInput = this.currentInput.slice(0, -1);
+        this.updateDisplay();
+    };
+    Calculator.prototype.calculate = function () {
+        if (this.currentInput === "" || this.previousInput === "" || this.currentOperation === null) {
+            return;
+        }
+        switch (this.currentOperation) {
+            case "+":
+                this.currentInput = (parseFloat(this.previousInput) + parseFloat(this.currentInput)).toString();
+                break;
+            case "-":
+                this.currentInput = (parseFloat(this.previousInput) - parseFloat(this.currentInput)).toString();
+                break;
+            case "*":
+                this.currentInput = (parseFloat(this.previousInput) * parseFloat(this.currentInput)).toString();
+                break;
+            case "÷":
+                if (this.currentInput === "0") {
+                    this.currentInput = "Error - Division by zero ";
+                }
+                else {
+                    this.currentInput = (parseFloat(this.previousInput) / parseFloat(this.currentInput)).toString();
+                }
+                break;
+            default:
+                this.currentInput = "Error";
+                break;
+        }
+        this.previousInput = "";
+        this.currentOperation = null;
+        this.updateDisplay();
+    };
+    return Calculator;
 }());
-var aa = new Button(1, 'a');
+var calculator = new Calculator();
--- ClientSide/typescript/src/main.ts
+++ ClientSide/typescript/src/main.ts
@@ -1,57 +1,130 @@
-interface iBtn {
-    studentId: number;
-    name: string;
+class Calculator {
+    // initailize variables
+    private currentInput: string = "";
+    private previousInput: string = "";
+    private currentOperation: string | null = null;
+    private currentOperand: HTMLDivElement | null = null;
+    private previousOperand: HTMLDivElement | null = null;
+
+    // constructor
+    constructor() {
+        // get elements
+        this.currentOperand = document.getElementById("current-operand") as HTMLDivElement;
+        this.previousOperand = document.getElementById("previous-operand") as HTMLDivElement;
+
+        const clearButton = document.getElementById("clear") as HTMLButtonElement;
+        const deleteButton = document.getElementById("delete") as HTMLButtonElement;
+        const operationButtons = document.querySelectorAll(".operation-button") as NodeListOf<HTMLButtonElement>;
+        const numberButtons = document.querySelectorAll(".number-button") as NodeListOf<HTMLButtonElement>;
+        const calculateButton = document.getElementById("calculate") as HTMLButtonElement;
+
+        this.updateDisplay();
+
+        // Button click
+        numberButtons.forEach((button) => {
+            button.addEventListener("click", () => {
+                this.numberButtonClick(button.textContent || "");
+            });
+        });
+
+        operationButtons.forEach((button) => {
+            button.addEventListener("click", () => {
+                this.operationButtonClick(button.textContent || "");
+            });
+        });
+
+        clearButton.addEventListener("click", () => {
+            this.clear();
+        });
+
+        deleteButton.addEventListener("click", () => {
+            this.delete();
+        });
+
+        calculateButton.addEventListener("click", () => {
+            this.calculate();
+        });
+    }
+
+    private updateDisplay() {
+        if (this.currentOperand && this.previousOperand) {
+            this.currentOperand.textContent = this.currentInput;
+            if (this.currentOperation) {
+                this.previousOperand.textContent = `${this.previousInput} ${this.currentOperation}`;
+            } else {
+                this.previousOperand.textContent = "";
+            }
+        }
+    }
+
+    private numberButtonClick(value: string) {
+        if (this.currentInput === "0" && value === "0") {
+            return;
+        }
+        this.currentInput += value;
+        this.updateDisplay();
+    }
+
+    private operationButtonClick(operation: string) {
+        if (this.currentInput === "") {
+            return;
+        }
+        if (this.currentOperation) {
+            this.previousInput = this.currentInput;
+            this.currentInput = "";
+            this.currentOperation = operation;
+            this.updateDisplay();
+        } else {
+            this.currentOperation = operation;
+            this.previousInput = this.currentInput;
+            this.currentInput = "";
+            this.updateDisplay();
+        }
+    }
+
+    private clear() {
+        this.currentInput = "";
+        this.previousInput = "";
+        this.currentOperation = null;
+        this.updateDisplay();
+    }
+
+    private delete() {
+        this.currentInput = this.currentInput.slice(0, -1);
+        this.updateDisplay();
+    }
+
+    private calculate() {
+        if (this.currentInput === "" || this.previousInput === "" || this.currentOperation === null) {
+            return;
+        }
+
+        switch (this.currentOperation) {
+            case "+":
+                this.currentInput = (parseFloat(this.previousInput) + parseFloat(this.currentInput)).toString();
+                break;
+            case "-":
+                this.currentInput = (parseFloat(this.previousInput) - parseFloat(this.currentInput)).toString();
+                break;
+            case "*":
+                this.currentInput = (parseFloat(this.previousInput) * parseFloat(this.currentInput)).toString();
+                break;
+            case "÷":
+                if (this.currentInput === "0") {
+                    this.currentInput = "Error - Division by zero ";
+                } else {
+                    this.currentInput = (parseFloat(this.previousInput) / parseFloat(this.currentInput)).toString();
+                }
+                break;
+            default:
+                this.currentInput = "Error";
+                break;
+        }
+
+        this.previousInput = "";
+        this.currentOperation = null;
+        this.updateDisplay();
+    }
 }
 
-let btnArray: any = [];
-
-class Button implements iBtn{
-    btn = document.getElementById('btnTs');
-    
-    constructor(public studentId: number, public name: string) {
-        this.btn?.addEventListener('click', this.create);
-    }
-    
-    private create() {
-        const input = document.getElementById('inputTs');
-        const tableBody = document.getElementById('tableBody');
-
-        const studentId = btnArray.length + 1;
-        const name = `${input?.value}`;
-
-        //New Row
-        const newRow = document.createElement('tr');
-        newRow.innerHTML = `
-            <td>${studentId}</td>
-            <td>${name}</td>
-            <td><button class="editBtn">Edit</button></td>
-            <td><button class="deleteBtn">Delete</button></td>
-        `;
-
-        //Edit Button
-        const editBtn = newRow.querySelector('.editBtn');
-        if (editBtn) {
-            editBtn.addEventListener('click', () => {
-                console.log("Edit");
-            });
-        }
-
-        //Delete Button
-        const deleteBtn = newRow.querySelector('.deleteBtn');
-        if (deleteBtn) {
-            deleteBtn.addEventListener('click', () => {
-                console.log("Delete");
-            });
-        }
-
-        //Append Row
-        if (tableBody) {
-            console.log("hej");
-            tableBody.appendChild(newRow);
-        }
-
-        const newBtn = new Button(studentId, name);
-        btnArray.push(newBtn);
-    }
-}
-const aa = new Button(1, 'a');
+const calculator = new Calculator();
